be1b51314c23b56b12e697091c600c45d77c2812
srabonx <srabon9012@gmail.com>
2024-07-16 01:19:52 +0600

    defined connection string

---
+++ Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MultiWeb.Models
+{
+	public class Category
+	{
+		[Key] public int Id { get; set; }
+		[Required] public string Name { get; set; }
+		public int DisplayOrder { get; set; }
+	}
+}

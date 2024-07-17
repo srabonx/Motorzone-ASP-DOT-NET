954afc9274f034c8cf894475304d3dcbb3011958
srabonx <srabon9012@gmail.com>
2024-07-17 23:58:58 +0600

    Fetch data from DB

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiWeb.Data;
 
 namespace MultiWeb.Controllers
 {
 	public class CategoryController : Controller
 	{
+		private readonly ApplicationDbContext m_db;
+
+		public CategoryController(ApplicationDbContext db)
+		{
+			m_db = db;
+		}
 		public IActionResult Index()
 		{
-			return View();
+			var objCategoryList = m_db.Categories.ToList();
+			return View(objCategoryList); 
 		}
 	}
 }

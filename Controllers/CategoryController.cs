791b4e02188937374c93f13c4a03ac4241754183
srabonx <srabon9012@gmail.com>
2024-07-27 00:48:28 +0600

    Revert "Error Building"
    This reverts commit 88c0d0ef63fd5687b2722e18b348663b27b2e825.

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -7,80 +7,80 @@ namespace MultiWeb.Controllers;
 
 public class CategoryController : Controller
 {
-    private readonly IUnitOfWork m_unitOfWork;
+	private readonly IUnitOfWork m_unitOfWork;
 
-    public CategoryController(IUnitOfWork unitOfWork)
-    {
-        m_unitOfWork = unitOfWork;
-    }
-    public IActionResult Index()
-    {
-        var objCategoryList = m_unitOfWork.CategoryRepository.GetAll().ToList();
-        return View(objCategoryList);
-    }
+	public CategoryController(IUnitOfWork unitOfWork)
+	{
+		m_unitOfWork = unitOfWork;
+	}
+	public IActionResult Index()
+	{
+		var objCategoryList = m_unitOfWork.CategoryRepository.GetAll().ToList();
+		return View(objCategoryList); 
+	}
 
-    public IActionResult Create()
-    {
-        return View();
-    }
+	public IActionResult Create()
+	{
+		return View();
+	}
 
-    [HttpPost]
-    public IActionResult Create(Category obj)
-    {
+	[HttpPost]
+	public IActionResult Create(Category obj)
+	{
+		
+		if (ModelState.IsValid)
+		{
+			m_unitOfWork.CategoryRepository.Add(obj);
+			m_unitOfWork.Save();
+			TempData["success"] = "Category created successfully";
+			return RedirectToAction("Index");
+		}
+		return View();
+	}
 
-        if (ModelState.IsValid)
-        {
-            m_unitOfWork.CategoryRepository.Add(obj);
-            m_unitOfWork.Save();
-            TempData["success"] = "Category created successfully";
-            return RedirectToAction("Index");
-        }
-        return View();
-    }
+	public IActionResult Edit(int? id)
+	{
+		if (id == null || id == 0) return NotFound();
 
-    public IActionResult Edit(int? id)
-    {
-        if (id == null || id == 0) return NotFound();
+		Category? categoryFromDb = m_unitOfWork.CategoryRepository.Get(e => e.Id == id);
 
-        Category? categoryFromDb = m_unitOfWork.CategoryRepository.Get(e => e.Id == id);
+		if(categoryFromDb == null) return NotFound();
 
-        if (categoryFromDb == null) return NotFound();
+		return View(categoryFromDb);
+	}
 
-        return View(categoryFromDb);
-    }
+	[HttpPost]
+	public IActionResult Edit(Category obj)
+	{
 
-    [HttpPost]
-    public IActionResult Edit(Category obj)
-    {
+		if (ModelState.IsValid)
+		{
+			m_unitOfWork.CategoryRepository.Update(obj);
+			m_unitOfWork.Save();
+			TempData["success"] = "Category updated successfully";
+			return RedirectToAction("Index");
+		}
+		return View();
+	}
 
-        if (ModelState.IsValid)
-        {
-            m_unitOfWork.CategoryRepository.Update(obj);
-            m_unitOfWork.Save();
-            TempData["success"] = "Category updated successfully";
-            return RedirectToAction("Index");
-        }
-        return View();
-    }
+	public IActionResult Delete(int? id)
+	{
+		if (id == null || id == 0) return NotFound();
 
-    public IActionResult Delete(int? id)
-    {
-        if (id == null || id == 0) return NotFound();
+		Category? categoryFromDb = m_unitOfWork.CategoryRepository.Get(e => e.Id == id);
 
-        Category? categoryFromDb = m_unitOfWork.CategoryRepository.Get(e => e.Id == id);
+		if (categoryFromDb == null) return NotFound();
 
-        if (categoryFromDb == null) return NotFound();
+		return View(categoryFromDb);
+	}
 
-        return View(categoryFromDb);
-    }
-
-    [HttpPost]
-    public IActionResult Delete(Category obj)
-    {
-        m_unitOfWork.CategoryRepository.Remove(obj);
-        m_unitOfWork.Save();
-        TempData["success"] = "Category deleted successfully";
-        return RedirectToAction("Index");
-    }
+	[HttpPost]
+	public IActionResult Delete(Category obj)
+	{
+		m_unitOfWork.CategoryRepository.Remove(obj);
+		m_unitOfWork.Save();
+		TempData["success"] = "Category deleted successfully";
+		return RedirectToAction("Index");
+	}
 
 }

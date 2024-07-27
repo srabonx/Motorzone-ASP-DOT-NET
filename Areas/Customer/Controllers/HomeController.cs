9feeeb88f8340d21fee0ac0091e24ab54cf76d34
srabonx <srabon9012@gmail.com>
2024-07-27 18:49:31 +0600

    Applied MVC area

--- Areas/Customer/Controllers/HomeController.cs
+++ Areas/Customer/Controllers/HomeController.cs
@@ -2,8 +2,9 @@ using Microsoft.AspNetCore.Mvc;
 using MultiWeb.Models;
 using System.Diagnostics;
 
-namespace MultiWeb.Controllers
+namespace MultiWeb.Areas.Customer.Controllers
 {
+    [Area("Customer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;

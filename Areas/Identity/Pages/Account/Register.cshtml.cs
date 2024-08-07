9129941f34a2f2a1ae507c834157190b5a65301b
srabonx <srabon9012@gmail.com>
2024-08-07 23:46:02 +0600

    Login/Logout UI imrpovement

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -4,6 +4,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
@@ -111,6 +112,18 @@ namespace MultiWeb.Areas.Identity.Pages.Account
 
             [ValidateNever]
             public IEnumerable<SelectListItem> RoleList { get; set; }
+
+
+            [Display(Name = "Street Address")]
+            public string? StreetAddress { get; set; }
+            public string? City { get; set; }
+            public string? Division { get; set; }
+
+            [Display(Name = "Postal Code")]
+            public string? PostalCode { get; set; }
+
+            [Display(Name = "Phone Number")]
+            public string? PhoneNumber { get; set; }
 
         }
 

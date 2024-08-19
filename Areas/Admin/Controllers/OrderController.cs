252d1ad4ae5714c7e20953acde98a248de440d7c
srabonx <srabon9012@gmail.com>
2024-08-20 01:48:37 +0600

    Order List page

---
+++ Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Multi.DataAccess.Repository.IUnitOfWorks;
+using Multi.Models;
+using Multi.Utility;
+using System.Diagnostics;
+
+namespace MultiWeb.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	[Authorize(Roles = StaticData.Identity_Role_Admin)]
+	public class OrderController : Controller
+	{
+
+		private readonly IUnitOfWork m_unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            m_unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+		{
+
+			return View();
+		}
+
+
+		#region API CALLS
+		[HttpGet]
+		public IActionResult GetAll(string status)
+		{
+			IEnumerable<OrderHeader> orderHeaders = m_unitOfWork.OrderHeaderRepo.GetAll(includeProp : "ApplicationUser").ToList();
+
+            switch (status)
+            {
+                case "pending":
+                    orderHeaders = orderHeaders.Where(u => u.PaymentStatus == StaticData.PaymentStatusDelayedPayment).ToList();
+                    break;
+                case "inprocess":
+                    orderHeaders = orderHeaders.Where(u => u.OrderStatus == StaticData.StatusInProcess).ToList();
+                    break;
+                case "completed":
+                    orderHeaders = orderHeaders.Where(u => u.OrderStatus == StaticData.StatusShipped).ToList();
+                    break;
+                case "approved":
+                    orderHeaders = orderHeaders.Where(u => u.OrderStatus == StaticData.StatusApproved).ToList();
+                    break;
+                default:
+                    break;
+            }
+
+            return Json(new { data = orderHeaders });
+		}
+
+
+		#endregion
+
+	}
+
+}
---
+++ wwwroot/js/order.js
@@ -0,0 +1,57 @@
+﻿
+var dataTable;
+
+$(document).ready(function () {
+
+    var url = window.location.search;
+
+    if (url.includes("inprocess")) {
+        loadDataTable("inprocess");
+    }
+    else if (url.includes("completed")) {
+        loadDataTable("completed");
+    }
+    else if (url.includes("pending")) {
+        loadDataTable("pending");
+    }
+    else if (url.includes("approved")) {
+        loadDataTable("approved");
+    }
+    else {
+        loadDataTable();
+    }
+        
+});
+
+function loadDataTable(status) {
+    dataTable = $('#order-list-table').DataTable(
+        {
+            "ajax": { url: '/admin/Order/getall?status=' + status },
+
+            "columns":
+                [
+                    { data: 'id', "width": "auto" },
+                    { data: 'applicationUser.name', "width": "auto" },
+                    { data: 'applicationUser.phoneNumber', "width": "auto" },
+                    { data: 'applicationUser.email', "width": "auto" },
+                    { data: 'orderStatus', "width": "auto" },
+                    { data: 'orderTotal', "width": "auto" },
+                    {
+                        data: 'id',
+                        "render": function (data) {
+                            return `<div class="btn-group" role="group">
+                                    <a href="/admin/Order/details?orderId=${data}" class="btn btn-outline-primary mx-2">
+                                        <i class="bi bi-pencil-square"></i> Details
+                                    </a>
+                                </div > `
+                        },
+                        "width": "auto"
+                    }
+                ]
+
+        }
+    );
+}
+
+
+
